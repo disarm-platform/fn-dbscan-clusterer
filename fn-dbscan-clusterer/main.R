c36849c7e1b6393714555ab64ff04c6cf035b796
Jonathan Smith <jonathan@peoplesized.com>
2019-01-28 15:50:37 +0200

    proper Dockerfile, don’t commit test data, rename to disarm container, etc.

--- fn-dbscan-clusterer/main.R
+++ fn-dbscan-clusterer/main.R
@@ -7,8 +7,7 @@ run_function = dget('function/function.R')
 main = function () {
   tryCatch({
     # reads STDIN as JSON, return error if any problems
-    # params = fromJSON(readLines(file("stdin")))
-    params = fromJSON(readLines(file("function/test_req.json")))
+    params = fromJSON(readLines(file("stdin")))
     
     # checks for existence of required parameters, return error if any problems
     # checks types/structure of all parameters, return error if any problems
@@ -44,7 +43,6 @@ handle_error = function(error) {
 handle_success = function(content) {
   type = 'success'
   function_response = as.json(list(type = unbox(type), content = content))
-  return(writeLines(function_response, "output.json"))
   return(write(function_response, stdout()))
 }
 
