8baaad5be3080286c07dab783ba78725b0c28ff3
Hugh Sturrock <hughsturrock@gmail.com>
2019-01-31 15:34:38 +0200

    Updated params tests

--- fn-dbscan-clusterer/function/params_tests.R
+++ fn-dbscan-clusterer/function/params_tests.R
@@ -1,12 +1,41 @@
 function(params) {
   # Individual check for each parameter
-  # if (is.null(params[['number']])) {
-  #   stop('Missing `number` parameter')
-  # }
-  # if (!is.numeric(params[['number']])) {
-  #   stop('Parameter `number` is not numeric')
-  # }
-  # if (length(params[['number']]) != 1) {
-  #   stop('Only pass a single value for `number')
-  # }
+  if (is.null(params[['subject']])) {
+    stop('Missing `subject` parameter')
+  }
+  
+  if (is.null(params[['max_num']])) {
+    stop('Missing `max_num` parameter')
+  }
+  
+  if (is.null(params[['max_dist_m']])) {
+    stop('Missing `max_dist_m` parameter')
+  }
+  
+  if (params[['max_num']] <= 0) {
+    stop("'max_num' should be > 0")
+  }
+  
+  if (params[['max_dist_m']] < 0) {
+    stop("'max_dist_m' should be >= 0")
+  }
+  
+  if (!is.numeric(params[['max_num']])) {
+    stop("'max_num' should numeric integer")
+  }
+  
+  if (!is.numeric(params[['max_dist_m']])) {
+    stop("'max_num' should numeric")
+  }
+  
+  if (length(params[['max_dist_m']])>1) {
+    stop("'max_dist_m' should single parameter")
+  }
+  
+  if (length(params[['max_num']])>1) {
+    stop("'max_dist_m' should single parameter")
+  }
+  
+  
+
 }
