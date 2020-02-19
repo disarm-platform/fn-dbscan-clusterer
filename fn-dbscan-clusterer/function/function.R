e4b8eec6be9835672d2658367dac78ca37187860
Jonathan Smith <jonathan@peoplesized.com>
2020-02-19 12:21:12 +0200

    fix double-nesting closes #8

--- fn-dbscan-clusterer/function/function.R
+++ fn-dbscan-clusterer/function/function.R
@@ -5,8 +5,5 @@ function(params) {
   # run function and catch result
   result = dbscan_function(params)
 
-  # wrap up result to match output structure from docs
-  response = list(result = result)
-
-  return(response)
+  return(result)
 }
